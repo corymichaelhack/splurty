bcc8c20f2f82d959b799a190b5075d1304d4be67
Cory Hack <cory.hack@gmail.com>
2017-12-21 20:35:25 +0000

    destoryed test quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,16 +1,19 @@
 class QuotesController < ApplicationController
+
 	def index
     @quote = Quote.order("RANDOM()").first
 	end
 
 
-
+  #Define method for creating a quote
   def create
+    # set variable equal to creating a new quote with params
     @quote = Quote.create(quote_params)
+    # if create quote is invalid
     if @quote.invalid?
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
     end
-
+    # if create quote is valid
     redirect_to root_path
 
   end
@@ -21,6 +24,7 @@ class QuotesController < ApplicationController
 
   private
 
+ #Define method for quote params
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
  root 'quotes#index'
+
+#Create quotes resouces
  resources :quotes
+
+
  get 'about' , to: 'quotes#about'
 end
