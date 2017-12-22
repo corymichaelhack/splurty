097b418197955fd11559b3e67ce0adf844456be6
Cory Hack <cory.hack@gmail.com>
2017-12-22 04:08:21 +0000

    fix font bug with google fonts

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,15 +5,15 @@ class QuotesController < ApplicationController
 	end
 
 
-  #Define method for creating a quote
+
   def create
-    # set variable equal to creating a new quote with params
+   
     @quote = Quote.create(quote_params)
-    # if create quote is invalid
+    
     if @quote.invalid?
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
     end
-    # if create quote is valid
+   
     redirect_to root_path
 
   end
@@ -24,7 +24,7 @@ class QuotesController < ApplicationController
 
   private
 
- #Define method for quote params
+
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-
  root 'quotes#index'
-
-#Create quotes resouces
  resources :quotes
-
-
  get 'about' , to: 'quotes#about'
 end
